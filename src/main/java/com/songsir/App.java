dda8e538e1ecd7571a5978770e088bc39a52d16c
songsir01 <1126563633@qq.com>
2019-12-25 15:21:24 +0800

    add redis test

--- src/main/java/com/songsir/App.java
+++ src/main/java/com/songsir/App.java
@@ -1,4 +1,4 @@
-package com.songsir.rpc.consumer;
+package com.songsir;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/songsir/rpc/consumer/controller/TestProviderController.java
+++ src/main/java/com/songsir/rpc/consumer/controller/TestProviderController.java
@@ -1,6 +1,7 @@
 package com.songsir.rpc.consumer.controller;
 
 import com.songsir.rpc.consumer.service.TestConsumerService;
+import com.songsir.rpc.utils.MyRedisTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,21 @@ public class TestProviderController {
 
     @Autowired
     private TestConsumerService testConsumerService;
+    @Autowired
+    private MyRedisTemplate redisTemplate;
 
     @RequestMapping("testConsumer")
     public String testConsumer() {
         return testConsumerService.getHelloWorld("");
+    }
+
+    @RequestMapping("testRedis")
+    public String testRedis() {
+        boolean set = redisTemplate.set("songsir", "songsirone", 100);
+        System.out.println(set);
+        Object songsir = redisTemplate.get("songsir");
+        System.out.println(songsir);
+        return "success";
     }
 
 }
